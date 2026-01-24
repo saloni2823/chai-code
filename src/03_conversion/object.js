279cc45d5eb50775cd909f0a57f3f74220960981
Saloni <salonipanchal778@gmail.com>
2026-01-24 16:32:22 +0000

    Auto-commit: 2026-01-24 16:32:22

--- src/03_conversion/object.js
+++ src/03_conversion/object.js
@@ -282,11 +282,4 @@ console.log(false - true); // -1 because false is converted to 0 and true to 1, 
 console.log(null + 1); // 1 because null is converted to 0 and then addition is performed
 console.log(undefined + 1); // NaN because undefined is converted to NaN and then addition is performed                 
 //types of data types in js
-//number
-//bigint
-//string
-//boolean
-//null
-//undefined
-//symbol
-//objec
+//p
