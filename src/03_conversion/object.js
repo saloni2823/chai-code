682a36cf0e8d34101f7120134c1330f1f573a99d
Saloni <salonipanchal778@gmail.com>
2026-01-24 16:33:06 +0000

    Auto-commit: 2026-01-24 16:33:06

--- src/03_conversion/object.js
+++ src/03_conversion/object.js
@@ -281,7 +281,7 @@ console.log(false + true); // 1 because false is converted to 0 and true to 1, t
 console.log(false - true); // -1 because false is converted to 0 and true to 1, then subtraction is performed
 console.log(null + 1); // 1 because null is converted to 0 and then addition is performed
 console.log(undefined + 1); // NaN because undefined is converted to NaN and then addition is performed                 
-//*******************types of data types in js
+//*******************types of data types in js***************************************
 //primitive data types:
 //1. number
 //2. bigint
